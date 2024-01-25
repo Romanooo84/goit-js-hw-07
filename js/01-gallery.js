8e0391377fc2c9c75aee4c3fd841c58ae32bf7ab
Pisarski Roman <pisarski.roman@gmail.com>
2024-01-25 20:36:29 +0100

    esc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,22 +1,50 @@
 import { galleryItems } from './gallery-items.js';  
 
 
-const images=galleryItems
 const list = document.querySelector(".gallery");
 //mapuje images i tworzę ciąg znaków z kodem html. Dla src i alt pobieram dane z tabllicy images. join łaczy tablice w ciąg znaków
-const markup = images.map(image => `<li><img class="gallery__link gallery__image" src="${image.preview}" alt="${image.description}"></li>`).join('');
+const markup = galleryItems.map(image =>
+	`<li class="gallery__item">
+		<a class="gallery__link" href='${image.original}'>
+			<img class="gallery__image"
+				src='${image.preview}'
+				data-source='${image.original}'
+				alt='${image.description}'
+			/>
+  		</a>
+	</li>`)
+	.join('');
 //tworzy elementy html wylistowane w markup
 list.insertAdjacentHTML("beforeend", markup);
 //sprawdza czy kliknieto w jakikolwiek element znajdujacy sie w ich przodku z klasą gallery 
 document.querySelector('.gallery').onclick = (event) => {
+	event.preventDefault()
 	//sprawdza jaki element został klikinięty
 	const imgElement = event.target;
 	//wyswietla alt tego elementu alt="${image.description}
 	const idElement = imgElement.alt;
 	//wybiera link do duzego zdjęcia z galerii dla wybranego id
 	const fidnLink = galleryItems.find(option => option.description === idElement).original;
+
 	//tworzenie okna modalnego z linkiem
-	basicLightbox.create(`
-		<img class="gallery__image"src="${fidnLink}">
-	`).show()
+	const instance = basicLightbox.create(
+		`<img src="${fidnLink}">`)
+	// wyświetlenie okna modalnego
+	instance.show()
+	
+	// nasłuchiwanie czy wcisniety jest przycisk esc i uruchomienie fukcji
+	window.addEventListener('keydown', onEscape)
+	
+	// funckcja zamykająca okno modalne po wcisnięciu esc
+	function onEscape(event) {
+		if (event.key === 'Escape')
+			instance.close()
+			window.removeEventListener('keydown', onEscape)
+		}
 }
+
+
+
+
+	
+
